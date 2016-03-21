2f67b076dd3b223ca01ebefb492f99b5e4b53221
Paul Boone <paulboone@pitt.edu>
2016-03-21 15:42:39 -0400

    echo each command to console

--- configuration/redis/install.sh
+++ configuration/redis/install.sh
@@ -4,8 +4,11 @@
 # cp ../../local/redis/3.0.7/bin/* /usr/local/bin/
 #
 
+set -ex
+
 echo "redis must already be installed prior to this. see script for details."
 
 cp ./redis.conf /etc/redis.conf
 cp redis.init /etc/init.d/redis
 chkconfig --add redis
+
